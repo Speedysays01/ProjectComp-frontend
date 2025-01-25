a9caa71695096b08eb5711ecda51837239837c35
Speedysays01 <surabhimhamane05@gmail.com>
2025-01-26 02:03:26 +0530

    added guidelines

--- src/Pages/guidelines/Guidelines.jsx
+++ src/Pages/guidelines/Guidelines.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
-import styles from './Guidelines.module.css'
+import React from "react";
+import { useInView } from "react-intersection-observer"; // For scroll tracking
+import styles from "./Guidelines.module.css"; // Modular CSS
 
 const Guidelines = () => {
-  return (
-    <div className={styles.container}>Guidelines</div>
-  )
-}
+  const { ref, inView } = useInView({
+    triggerOnce: false, // Allow the animation to trigger multiple times as we scroll
+    threshold: 0.35, // Trigger when 35% of the section is visible
+  });
 
-export default Guidelines
+  return (
+    <div ref={ref} className={styles.container}>
+      <h3>GUIDELINES</h3>
+      <div
+        className={`${styles.boxCyan} ${inView ? styles.slideInLeft : styles.slideOutLeft}`}
+      ></div>
+      <div
+        className={`${styles.boxOrange} ${inView ? styles.slideInRight : styles.slideOutRight}`}
+      ></div>
+      <div
+        className={`${styles.boxCyan} ${inView ? styles.slideInLeft : styles.slideOutLeft}`}
+      ></div>
+      <div
+        className={`${styles.boxOrange} ${inView ? styles.slideInRight : styles.slideOutRight}`}
+      ></div>
+      <div
+        className={`${styles.boxCyan} ${inView ? styles.slideInLeft : styles.slideOutLeft}`}
+      ></div>
+      <div
+        className={`${styles.boxOrange} ${inView ? styles.slideInRight : styles.slideOutRight}`}
+      ></div>
+    </div>
+  );
+};
+
+export default Guidelines;
